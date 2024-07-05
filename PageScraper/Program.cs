5f5ca1f0ebbebbb395c6050582747ab9ee34a399
Mohammad Jabir <mohammadj94@gmail.com>
2024-07-05 13:05:47 -0600

    Initial commit. Set up of html and selenium scrapers.

---
+++ PageScraper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PageScraper;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        HTTP_PageScraper scraper = new HTTP_PageScraper();
+        await scraper.Scrape("https://www.markaz.ca/");
+    }
+}
